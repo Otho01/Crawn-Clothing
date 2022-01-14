e92dd7f501cad90c5744797cb81925e95c1e2bc7
Luis Gonzalez Cortes <luis.gonzalezc@perficient.com>
2022-01-13 22:55:53 -0500

    updated our menu-items to navigate with the withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div className='App'>
-      <HomePage />
+      <Route exact path='/' component={HomePage} />
     </div>
   );
 }
