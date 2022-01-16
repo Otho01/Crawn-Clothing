1ff390e56963506e5d512bfe77410b36b9ef984a
Luis Gonzalez Cortes <luis.gonzalezc@perficient.com>
2022-01-16 17:02:20 -0500

    updating app with shop page, collection-preview component and collection-item component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div className='App'>
       <Route exact path='/' component={HomePage} />
+      <Route exact path='/shop' component={ShopPage} />
     </div>
   );
 }
